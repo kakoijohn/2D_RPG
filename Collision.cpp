42e24e04a32fa8d78be6628f134c5a022a6526d2
John Damits <kakoijohn@gmail.com>
2015-02-05 04:58:08 -0500

    Separating Axis Theorem collision detection.

---
+++ Collision.cpp
@@ -0,0 +1,9 @@
+//
+//  Collision.cpp
+//  2D_RPG
+//
+//  Created by John Damits on 2/5/15.
+//  Copyright (c) 2015 Squirrely Brace. All rights reserved.
+//
+
+#include "Collision.h"
---
+++ Collision.h
@@ -0,0 +1,14 @@
+//
+//  Collision.h
+//  2D_RPG
+//
+//  Created by John Damits on 2/5/15.
+//  Copyright (c) 2015 Squirrely Brace. All rights reserved.
+//
+
+#ifndef ___D_RPG__Collision__
+#define ___D_RPG__Collision__
+
+#include <stdio.h>
+
+#endif /* defined(___D_RPG__Collision__) */
